e2bfd57e3c4061e3d40d75313dcd37b5792c9c94
Adouani Feres <feresadouani10@gmail.com>
2023-05-18 21:53:44 +0100

    correction routes

--- GestionFaculte/routes/web.php
+++ GestionFaculte/routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\EmployesController;
+use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\ProfesseurController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +35,12 @@ Route::get('/students/signup',[StudentController::class, 'showSignUpForm'])->nam
 Route::post('/students/signup',[StudentController::class, 'processSignUpEtudiant'])->name('students.signup.submit');
 Route::get('loginStudent', [StudentController::class, 'loginStudent'])->name('loginStudent');
 Route::post('postloginStudent', [StudentController::class, 'postloginStudent'])->name('postloginStudent'); 
- 
+Route::get('/professeur/etudiant-list',[ProfesseurController::class, 'index']);
+//edit
+Route::get('/professeur/edit-student/{id}',[ProfesseurController::class,'editStudent']);
+Route::post('/professeur/save-student/{id}',[ProfesseurController::class,'updateStudent']);
+//delete
+Route::get('/professeur/delete-student/{id}',[ProfesseurController::class,'deleteStudent']);
 
 //admin route
 Route::prefix('admin')->middleware('auth')->group(function () {
