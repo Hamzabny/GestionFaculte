a808f662a63df98fa4a43d8924e06b3c5b73c5b7
hamza ben younes <hamzabenyoues199@gmail.com>
2023-05-18 15:00:19 +0100

    change login and registration login for student Design

--- GestionFaculte/resources/views/loginStudent.blade.php
+++ GestionFaculte/resources/views/loginStudent.blade.php
@@ -7,41 +7,97 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
 
     <title>Document</title>
+    <style>
+        body {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #f8f9fa;
+        }
+
+        .signup-form {
+            max-width: 400px;
+            width: 100%;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .signup-form h1 {
+            margin-bottom: 20px;
+            font-weight: bold;
+            font-size: 24px;
+            text-align: center;
+        }
+
+        .signup-form .btn-close {
+            margin-top: -10px;
+            color: #000;
+        }
+
+        .signup-form form {
+            margin-top: 15px;
+        }
+
+        .signup-form .form-floating {
+            margin-bottom: 15px;
+        }
+
+        .signup-form .form-control {
+            border-radius: 10px;
+            box-shadow: none;
+        }
+
+        .signup-form .btn-primary {
+            border-radius: 10px;
+            padding: 12px;
+        }
+
+        .signup-form small {
+            display: block;
+            margin-top: 10px;
+            font-size: 14px;
+            color: #6c757d;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    
-<div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content rounded-4 shadow">
-      <div class="modal-header p-5 pb-4 border-bottom-0">
-        <h1 class="fw-bold mb-0 fs-2">Sign up for free</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
+    <div class="signup-form">
+        <div class="container" role="document">
+            <div>
+                <div class="row justify-content-center">
+                    <h1 class="card-header text-center">Sign up for free</h1>
 
-      <div class="modal-body p-5 pt-0">
-        <form method="POST" action="{{ route('postloginStudent') }}">
-        @csrf
-          <div class="form-floating mb-3">
-            <input type="text" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com" name="email"     autofocus >
-            <label for="floatingInput">Email address</label>
-                                @if ($errors->has('email'))
-                                <span class="text-danger">{{ $errors->first('email') }}</span>
-                                @endif
-          </div>
-          <div class="form-floating mb-3">
-            <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" name="password">
-            <label for="floatingPassword">Password</label>
-            @if ($errors->has('password'))
-                                <span class="text-danger">{{ $errors->first('password') }}</span>
-                                @endif
-          </div>
-          <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Sign up</button>
-          <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
-        </form>
-      </div>
+                </div>
+
+                <div class="modal-body p-4 pt-0">
+                    <form method="POST" action="{{ route('postloginStudent') }}">
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com" name="email" autofocus>
+                            <label for="floatingInput">Email address</label>
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" name="password">
+                            <label for="floatingPassword">Password</label>
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                        <button class="btn btn-dark btn-block" type="submit">Sign up</button>
+                        <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- GestionFaculte/resources/views/registrationStudent.blade.php
+++ GestionFaculte/resources/views/registrationStudent.blade.php
@@ -5,61 +5,94 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            height: 100vh;
+            margin-top:100px;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .signup-form .card {
+            border-radius: 10px;
+        }
+
+        .signup-form .card-header {
+            background-color: #343a40;
+            color: #fff;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        .signup-form .form-group {
+            margin-bottom: 1.5rem;
+        }
+
+        .signup-form .form-control {
+            border-radius: 10px;
+        }
+
+        .signup-form .btn-block {
+            border-radius: 10px;
+        }
+    </style>
 </head>
 <body>
-<main class="signup-form">
-    <div class="cotainer">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <h3 class="card-header text-center">Register Student</h3>
-                    <div class="card-body">
-                        <form action="{{ route('students.signup.submit') }}" method="POST">
-                            @csrf
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Name" id="name" class="form-control" name="nom" autofocus>
-                                @if ($errors->has('nom'))
-                                <span class="text-danger">{{ $errors->first('nom') }}</span>
-                                @endif
-                            </div>
-                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Name" id="name" class="form-control" name="prenom" autofocus>
-                                @if ($errors->has('prenom'))
-                                <span class="text-danger">{{ $errors->first('prenom') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Email" id="email_address" class="form-control" name="email" autofocus>
-                                @if ($errors->has('email'))
-                                <span class="text-danger">{{ $errors->first('email') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="password" placeholder="Password" id="password" class="form-control" name="password">
-                                @if ($errors->has('password'))
-                                <span class="text-danger">{{ $errors->first('password') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="date" placeholder="Password" id="password" class="form-control" name="datedenaissance">
-                                @if ($errors->has('datedenaissance'))
-                                <span class="text-danger">{{ $errors->first('datedenaissance') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <div class="checkbox">
-                                    <label><input type="checkbox" name="remember"> Remember Me</label>
-                                </div>
-                            </div>
-                            <div class="d-grid mx-auto">
-                                <button type="submit" class="btn btn-dark btn-block">Sign up</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</main>
+    <main class="signup-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text-center">Register Student</h3>
+                        <div class="card-body">
+                            <form action="{{ route('students.signup.submit') }}" method="POST">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Name" id="name" class="form-control" name="nom" autofocus>
+                                    @if ($errors->has('nom'))
+                                    <span class="text-danger">{{ $errors->first('nom') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Last Name" id="lastname" class="form-control" name="prenom" autofocus>
+                                    @if ($errors->has('prenom'))
+                                    <span class="text-danger">{{ $errors->first('prenom') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email_address" class="form-control" name="email" autofocus>
+                                    @if ($errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="password" placeholder="Password" id="password" class="form-control" name="password">
+                                    @if ($errors->has('password'))
+                                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="date" placeholder="Date of Birth" id="dob" class="form-control" name="datedenaissance">
+                                    @if ($errors->has('datedenaissance'))
+                                    <span class="text-danger">{{ $errors->first('datedenaissance') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <div class="checkbox">
+                                        <label><input type="checkbox" name="remember"> Remember Me</label>
+                                    </div>
+                                </div>
+                                <div class="d-grid mx-auto">
+                                    <button type="submit" class="btn btn-dark btn-block">Sign up</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
