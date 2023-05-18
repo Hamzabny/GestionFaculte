dbc0e83b6ccfa32ed5c777df9e44125e551d7a1f
Naim Atia <atianaim34@gmail.com>
2023-05-18 19:59:57 +0100

    fix

--- GestionFaculte/routes/web.php
+++ GestionFaculte/routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\CustomAuthController;
-use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
